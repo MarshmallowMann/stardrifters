08bcd218ce7020251402c17251d67e87fd97df9d
JOHN ROBERTSON DESPI <64559253+MarshmallowMann@users.noreply.github.com>
2024-04-05 16:53:17 +0800

    chore: init repo
    using libgdx

---
+++ desktop/src/com/group5/stardrifters/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.group5.stardrifters;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.group5.stardrifters.Stardrifters;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("stardrifters");
+		new Lwjgl3Application(new Stardrifters(), config);
+	}
+}
